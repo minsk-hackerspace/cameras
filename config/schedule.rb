e1570579bd8364252c2408b02c1324fa789a1019
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2018-05-05 21:22:22 +0300

    paths fixed

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,6 @@ every 1.minute do
   command 'cd /home/ubuntu/cameras && `which ruby` /home/ubuntu/cameras/every_minute.rb'
 end
 
-# every 1.day, at: '0:01 am' do
-every 5.minutes do
+every 1.day, at: '0:01 am' do
   command 'cd /home/ubuntu/cameras && `which ruby` /home/ubuntu/cameras/daily.rb'
 end
--- every_minute.rb
+++ every_minute.rb
@@ -3,7 +3,11 @@ require_relative 'camera'
 camera_strings = File.readlines 'cameras'
 cameras = []
 camera_strings.each_with_index { |c, i| cameras << Camera.new(c, 'cam_' + i.to_s) }
-
 cameras.each { |camera| camera.snapshot }
+
 sleep 30
+
+camera_strings = File.readlines 'cameras'
+cameras = []
+camera_strings.each_with_index { |c, i| cameras << Camera.new(c, 'cam_' + i.to_s) }
 cameras.each { |camera| camera.snapshot }
